c393b27b5581da7dd0fd1bafe787e9236d8f449b
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-07-04 17:35:34 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# RKT--event-platform
+ Exercício de criação de uma plataforma de eventos, por Rocketseat
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <p className="p-2 text-5xl text-violet-500 font-bold">oi</p>
+    </div>
+  );
+}
+
+export default App;
