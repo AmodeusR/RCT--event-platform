2bcc1ada7e3b6419c88bfe684c10c422c62dc0a8
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-07-05 19:13:19 -0300

    Create Header.tsx

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import Logo
+ from "./Logo";
+const Header = () => {
+  return (
+    <>
+      <header className="flex py-5 justify-center bg-gray-700 border-b border-gray-600">
+        <Logo />
+      </header>
+    </>
+  );
+};
+
+export default Header;
