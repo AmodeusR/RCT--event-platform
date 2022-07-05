3f3c3b8c15a51eb6307128dbd817d8a92b46effb
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-07-05 18:16:37 -0300

    Created apollo client

---
+++ src/lib/apollo.ts
@@ -0,0 +1,16 @@
+import { ApolloClient, gql, InMemoryCache } from "@apollo/client";
+
+
+export const client = new ApolloClient({
+  uri: "https://api-sa-east-1.graphcms.com/v2/cl577l92j4drw01t36kl5bul3/master",
+  cache: new InMemoryCache()
+});
+
+export const GET_LESSONS_QUERY = gql`
+query {
+  lessons {
+    id
+    title
+  }
+}
+`
