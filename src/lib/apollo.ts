98af08ef84f6e96f18a893cddeb247470c1a562e
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-07-12 20:18:41 -0300

    Added new query by lesson

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -7,13 +7,28 @@ export const client = new ApolloClient({
 });
 
 export const GET_LESSONS_QUERY = gql`
-query {
-  lessons (orderBy: availableAt_ASC, stage: PUBLISHED) {
-    id
-    title
-    slug
-    availableAt
-    lessonType
+  query {
+    lessons (orderBy: availableAt_ASC, stage: PUBLISHED) {
+      id
+      title
+      slug
+      availableAt
+      lessonType
+    }
   }
-}
+`
+
+export const GET_LESSON_BY_SLUG = gql`
+  query GetLessonBySlug ($slug: String) {
+    lesson(where: {slug: $slug}) {
+      title
+      videoId
+      description
+      teacher {
+        avatarURL
+        bio
+        name
+      }
+    }
+  }
 `
